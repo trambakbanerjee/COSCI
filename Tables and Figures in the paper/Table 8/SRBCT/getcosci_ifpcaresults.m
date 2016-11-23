f2b8fb1fa402488f272a0dfb6160402aa477b084
trambakbanerjee <trambakb@usc.edu>
2016-11-22 18:02:35 -0800

    Add files via upload

---
+++ Tables and Figures in the paper/Table 8/SRBCT/getcosci_ifpcaresults.m
@@ -0,0 +1,34 @@
+
+load('selected.mat');
+load('srbct_y.mat');
+
+trueclass = srbcty;
+CER = zeros(30,1);
+K = 4;
+
+parfor rep=1:30
+    
+    G = selected*(selected');
+    [V, ~] = eigs(G, K - 1); 
+    label = kmeans(V, K, 'replicates', 30);
+    
+    predclass = label;
+    
+    % Calculate CER
+    
+    n = size(selected,1);
+    num = 0;
+    den = n*(n-1)/2;
+    for i= 1:(n-1)
+        
+        tt = predclass(i)-predclass(i+1:n);
+        tt(tt~=0)=1;
+        tt = 1-tt;
+        dd = trueclass(i)-trueclass(i+1:n);
+        dd(dd~=0)=1;
+        dd = 1-dd;
+        num = sum(abs(tt-dd))+num;
+        
+    end
+   CER(rep,:) = num/den;
+end
